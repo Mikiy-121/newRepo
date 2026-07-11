b622ebc8f6fe9b96143489cad969a71e55e38c30
Mikiyas teshale <mikiy256@gmail.com>
2026-07-11 11:19:30 +0300

    set

--- Day03/sets.py
+++ Day03/sets.py
@@ -1,5 +1,5 @@
-nums = [1, 2, 2, 3, 3, 3]
-unique = set(nums) # {1, 2, 3}
+nums = [1, 2, 2, 3, 3, 3,5,7,7]
+unique = set(nums) # {1, 2, 3, 5, 7}
 a = {"Almaz", "Dawit"}
 b = {"Dawit", "Samuel"}
 a | b # union -> {'Almaz', 'Dawit', 'Samuel'}

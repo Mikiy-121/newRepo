f672bb6ef93fbafbc9b87d8e2be5f5a260949c8d
Mikiyas teshale <mikiy256@gmail.com>
2026-07-14 11:14:17 +0300

    @property

---
+++ Day-04/@properties.py
@@ -0,0 +1,15 @@
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    @property
+    def balance(self): # getter
+        return self.__balance
+
+    @balance.setter
+    def balance(self, value): # setter
+        if value < 0:
+            raise ValueError("No negatives")
+        self.__balance = value
+
+       
